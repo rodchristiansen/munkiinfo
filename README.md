6ba54884d46c7855a15d9909444979754b2ebdd5
tuxudo <tuxudo@users.noreply.github.com>
2024-08-30 10:16:45 -0400

    Add munkiinfo widgets

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ The following information is stored in the munkiinfo table:
 
 _NOTE:_ the names line up with [Munki Preferences](https://github.com/munki/munki/wiki/Preferences) as such no description is given
 
+Munki Preference Keys
+------
 
 * AppleSoftwareUpdatesOnly
 * CatalogURL
@@ -46,9 +48,15 @@ _NOTE:_ the names line up with [Munki Preferences](https://github.com/munki/munk
 * UseClientCertificate
 * UseClientCertificateCNAsClientIdentifier
 
-
 These are the only non-ManagedInstall preference values stored in this module
 
 * AppleCatalogURL - This is the catalog that `/usr/sbin/softwareupdate` will use to find updates
 * munkiprotocol - This is the url scheme for the `SoftwareRepoURL` key. This is used for widgets. 
 * middlewarename and middlewareversion - These are displayed only if middleware is installed. Version relies on a get_version() function in your middleware script.
+
+
+Table Schema
+------
+
+munkiinfo_key - VARCHAR(255) - Munki preference key name
+munkiinfo_value - VARCHAR(255) - Munki preference key value
--- munkiinfo_controller.php
+++ munkiinfo_controller.php
@@ -10,33 +10,32 @@ class munkiinfo_controller extends Module_controller
     protected $module_path;
     protected $view_path;
 
-
-  /*** Protect methods with auth! ****/
+    /*** Protect methods with auth! ****/
     public function __construct()
     {
-      // Store module path
+        // Store module path
         $this->module_path = dirname(__FILE__);
         $this->view_path = dirname(__FILE__) . '/views/';
     }
 
-  /**
-   * Default method
-   *
-   * @author
-   **/
+    /**
+     * Default method
+     *
+     * @author
+     **/
     public function index()
     {
         echo "You've loaded the munkiinfo module!";
     }
 
     /**
-    * undocumented function summary
-    *
-    * Undocumented function long description
-    *
-    * @param type var Description
-    * @return {11:return type}
-    */
+     * undocumented function summary
+     *
+     * Undocumented function long description
+     *
+     * @param type var Description
+     * @return {11:return type}
+     */
     public function listing($value = '')
     {
         if (! $this->authorized()) {
@@ -48,37 +47,59 @@ class munkiinfo_controller extends Module_controller
         $obj->view('munkiprotocol_listing', $data, $this->view_path);
     }
 
-  /**
-   * Get Munki Protocol Statistics
-   *
-   * @author erikng
-   **/
+    /**
+     * Get Munki Protocol Statistics
+     *
+     * @author erikng
+     **/
     public function get_protocol_stats()
     {
-
         if (! $this->authorized()) {
-        // die('Authenticate first.'); // Todo: return json
             $out['error'] = 'Not authorized';
         }
 
-        $queryobj = new munkiinfo_model();
         $sql = "SELECT  COUNT(1) as total,
-                      COUNT(CASE WHEN `munkiinfo_key` = 'munkiprotocol' AND `munkiinfo_value` = 'http' THEN 1 END) AS http,
-                      COUNT(CASE WHEN `munkiinfo_key` = 'munkiprotocol' AND `munkiinfo_value` = 'https' THEN 1 END) AS https,
-                      COUNT(CASE WHEN `munkiinfo_key` = 'munkiprotocol' AND `munkiinfo_value` = 'localrepo' THEN 1 END) AS localrepo
-                       FROM munkiinfo
-                       LEFT JOIN reportdata USING (serial_number)
-                       ".get_machine_group_filter();
-        $obj = new View();
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+                        COUNT(CASE WHEN `munkiinfo_key` = 'munkiprotocol' AND `munkiinfo_value` = 'http' THEN 1 END) AS http,
+                        COUNT(CASE WHEN `munkiinfo_key` = 'munkiprotocol' AND `munkiinfo_value` = 'https' THEN 1 END) AS https,
+                        COUNT(CASE WHEN `munkiinfo_key` = 'munkiprotocol' AND `munkiinfo_value` = 'localrepo' THEN 1 END) AS localrepo
+                        FROM munkiinfo
+                        LEFT JOIN reportdata USING (serial_number)
+                        ".get_machine_group_filter();
+
+        $queryobj = new Munkiinfo_model;
+        jsonView($queryobj->query($sql)[0]);
     }
 
-  /**
-   * Get munki preferences for serial_number
-   *
-   * @param string $serial serial number
-   * @author clburlison
-   **/
+    /**
+     * Get data for scroll widget
+     *
+     * @return void
+     * @author tuxudo
+     **/
+    public function get_scroll_widget($column)
+    {
+        // Remove non-column name characters
+        $column = preg_replace("/[^A-Za-z0-9_\-]]/", '', $column);
+
+        $sql = "SELECT  `munkiinfo_value` as ".$column.",
+                        COUNT(`munkiinfo_value`) AS count
+                        FROM munkiinfo
+                        LEFT JOIN reportdata USING (serial_number)
+                        ".get_machine_group_filter()."
+                        AND `munkiinfo_key` = '".$column."' AND `munkiinfo_value` <> '' AND `munkiinfo_value` IS NOT NULL
+                        GROUP BY `munkiinfo_value`
+                        ORDER BY count DESC";
+
+        $queryobj = new Munkiinfo_model;
+        jsonView($queryobj->query($sql));
+    }
+
+    /**
+     * Get munki preferences for serial_number
+     *
+     * @param string $serial serial number
+     * @author clburlison
+     **/
     public function get_data($serial = '')
     {
         $out = array();
